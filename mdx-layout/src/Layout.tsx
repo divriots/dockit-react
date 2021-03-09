7d0d3118e6ad9d9b554d3d5b8e3053b6d4639175
Mihai <veres85@yahoo.com>
2021-03-09 19:23:48 +0000

    added font sizes for different headings

--- mdx-layout/src/Layout.tsx
+++ mdx-layout/src/Layout.tsx
@@ -7,18 +7,16 @@ import React, { ReactChildren, ReactChild } from 'react';
 export const Layout = ({
   __context,
   children,
-  articleClassName,
   ...props // MDX exports
 }: {
   __context: Context; // Layout Context
   children: ReactChild | ReactChildren; // MDX Content
-  articleClassName: string; // class to apply to <article>
 }) => {
   const graph: PageGraph = buildGraph(__context);
   return (
     <main>
       <NavBar graph={graph} {...props} />
-      <article className={articleClassName}>{children}</article>
+      <article>{children}</article>
     </main>
   );
 };

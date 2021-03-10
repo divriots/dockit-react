a014ccc16ee1a2aca4c8b4819a99be70feef6096
Mihai <veres85@yahoo.com>
2021-03-10 19:55:20 +0000

    removed link to prism tsx component - not needed

--- mdx-layout/src/Layout.tsx
+++ mdx-layout/src/Layout.tsx
@@ -17,11 +17,6 @@ export const Layout = ({
   const graph: PageGraph = buildGraph(__context);
   return (
     <main>
-      <script
-        src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/components/prism-tsx.min.js"
-        integrity="sha512-ueCte/ZwNI460xA3ipwvMk/emlgFYNZGBLlc3EMqZqJbQoLaOhoCpY1l6dHbfNz2p+A9E/sQoUdNRVvu/cqQEw=="
-        crossorigin="anonymous"
-      />
       <NavBar graph={graph} {...props} />
       <article className={articleClassName}>{children}</article>
     </main>

edd411322e599ab4112485f3447af519bb9ed98f
Mihai <veres85@yahoo.com>
2021-03-11 14:35:09 +0000

    core setup

--- mdx-layout-core/src/index.tsx
+++ mdx-layout-core/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import { Playground } from '~/playground/dist/Playground.js';
-import { Showcases } from '~/showcases/dist/Showcases.js';
-import { Props } from '~/props/dist/Props.js';
+import { Playground } from '~/playground';
+import { Showcases } from '~/showcases';
+import { Props, Description } from '~/props';
 import { Layout as LayoutComponent } from './Layout';
 
 export const Layout = (props) => (
-  <MDXProvider components={{ Playground, Props, Showcases }}>
+  <MDXProvider components={{ Playground, Props, Description, Showcases }}>
     <LayoutComponent {...props} />
   </MDXProvider>
 );
--- mdx-layout-css/src/index.tsx
+++ mdx-layout-css/src/index.tsx
@@ -3,8 +3,8 @@ import { MDXProvider } from '@mdx-js/react';
 import { Playground } from '~/playground/dist/Playground.js';
 import { Showcases } from '~/showcases/dist/Showcases.js';
 import { Props } from '~/props/dist/Props.js';
-import { Layout as LayoutComponent } from './Layout';
-import { StylesheetSwitch } from './StylesheetSwitch';
+import { Layout as LayoutComponent } from '~/mdx-layout-core/dist/Layout';
+import { StylesheetSwitch } from '~/mdx-layout-core/dist/StylesheetSwitch';
 
 export const Layout = (props) => (
   <MDXProvider components={{ Playground, Props, Showcases }}>

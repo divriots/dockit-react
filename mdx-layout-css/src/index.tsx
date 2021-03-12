13a5a6b0b38a32a8400d9f8ac717f74c673b496d
Mihai <veres85@yahoo.com>
2021-03-12 11:22:16 +0000

    tailwind layout list markers in the idiomatic way

--- mdx-layout-css/src/index.tsx
+++ mdx-layout-css/src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { Playground } from '~/playground';
 import { Showcases } from '~/showcases';
-import { Props } from '~/props';
+import { Props, Description } from '~/props';
 import { CoreLayout, StylesheetSwitch } from '~/mdx-layout-core';
 
 export const CssLayout = (props) => (
-  <MDXProvider components={{ Playground, Props, Showcases }}>
+  <MDXProvider components={{ Playground, Props, Showcases, Description }}>
     <StylesheetSwitch
       stylesheets={{
         light: new URL('light.css', import.meta.url).href,
--- mdx-layout-tailwind/src/index.tsx
+++ mdx-layout-tailwind/src/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { CoreLayout } from '~/mdx-layout-core';
+import { Playground } from '~/playground';
+import { Showcases } from '~/showcases';
+import { Props, Description } from '~/props';
 import './styles.css';
 import { setup } from 'twind/shim';
 import typography from '@twind/typography';
+
+const Ol = (props) => <ol class="list-decimal" {...props} />;
+const Ul = (props) => <ul class="list-disc" {...props} />;
+const Li = (props) => <li class="ml-4" {...props} />;
 
 // Setup tailwind
 setup({ plugins: { ...typography() } });
 
 export const TailwindLayout = (props) => (
-  <MDXProvider components={{}}>
+  <MDXProvider
+    components={{
+      ol: Ol,
+      ul: Ul,
+      li: Li,
+      Playground,
+      Showcases,
+      Props,
+      Description,
+    }}
+  >
     <CoreLayout
       {...props}
       articleClassName="prose prose-sm sm:prose lg:prose-lg xl:prose-xl"

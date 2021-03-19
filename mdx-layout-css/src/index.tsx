e057a66c56e4082d0c2902f1b3b6e9b41eacf295
Georges Gomes <georges.gomes@gmail.com>
2021-03-19 08:33:02 +0000

    improvements in layouts

--- mdx-layout-css/src/index.tsx
+++ mdx-layout-css/src/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import { Playground } from '~/playground';
-import { Showcases } from '~/showcases';
-import { Props, Description } from '~/props';
 import { CoreLayout, StylesheetSwitch } from '~/mdx-layout-core';
 
-
 export const CssLayout = ({ components = {}, onSwitch, ...rest }) => (
-  <MDXProvider components={{ Playground, Props, Description, Showcases, ...components }}>
+  <MDXProvider components={{ ...components }}>
     <StylesheetSwitch
       stylesheets={{
         light: 'https://cdn.jsdelivr.net/npm/water.css/out/light.min.css',

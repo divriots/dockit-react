5bff7250d7fc336db4318031e95e88c859809a58
Mihai <veres85@yahoo.com>
2021-05-11 09:10:16 +0000

    inline styles for mdx-layout-css

--- mdx-layout-css/src/index.tsx
+++ mdx-layout-css/src/index.tsx
@@ -2,6 +2,38 @@ import React from 'react';
 import { MDXProvider } from '@mdx-js/react';
 import { CoreLayout, StylesheetSwitch } from '~/mdx-layout-core';
 
+const styles = `
+h1 {
+  font-size: 2.25rem;
+}
+
+h2 {
+  font-size: 1.85rem;
+}
+
+h3 {
+  font-size: 1.55rem;
+}
+
+h4 {
+  font-size: 1.25rem;
+}
+
+h5 {
+  font-size: 1rem;
+}
+
+ol {
+  list-style-type: decimal;
+  padding-inline-start: 2rem;
+}
+
+ul {
+  padding-inline-start: 2rem;
+  list-style-type: disc;
+}
+`;
+
 export const CssLayout = ({ components = {}, onSwitch, ...rest }) => (
   <MDXProvider components={{ ...components }}>
     <StylesheetSwitch
@@ -11,7 +43,7 @@ export const CssLayout = ({ components = {}, onSwitch, ...rest }) => (
       }}
       onSwitch={onSwitch}
     />
-    <link rel="stylesheet" href={new URL('commons.css', import.meta.url).href} />
+    <style>{styles}</style>
     <CoreLayout {...rest} />
   </MDXProvider>
 );

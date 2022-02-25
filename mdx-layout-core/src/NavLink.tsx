d4fee5e6497a944817a4951368ef38d8d1f206a5
Mikhail Bashkirov <bashmish@gmail.com>
2022-02-25 18:28:57 +0100

    chore(mdx-layout): do quick exit and decrease code indentation

--- mdx-layout-core/src/NavLink.tsx
+++ mdx-layout-core/src/NavLink.tsx
@@ -12,31 +12,32 @@ const relativeUrl = (url: string) =>
   );
 
 export const NavLink = ({ page }: { page: Page }) => {
+  if (!page.url) {
+    return null;
+  }
   const [hover, setHover] = React.useState(false);
 
   return (
-    page.url && (
-      <a
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        className="layout-nav-page"
-        href={relativeUrl(page.url)}
-        style={{
-          ...styles.listItem,
-          ...(location.href.endsWith(page.url) && {
-            fontWeight: 'bold',
-          }),
-          ...((location.href.endsWith(page.url) || hover) && {
-            opacity: '1',
-            backgroundColor: 'hsla(0, 0%, 50%, 0.15)',
-          }),
-        }}
-        {...(location.href.endsWith(page.url) && {
-          'aria-current': 'location',
-        })}
-      >
-        {(page.nav && page.nav.key) || page.url}
-      </a>
-    )
+    <a
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className="layout-nav-page"
+      href={relativeUrl(page.url)}
+      style={{
+        ...styles.listItem,
+        ...(location.href.endsWith(page.url) && {
+          fontWeight: 'bold',
+        }),
+        ...((location.href.endsWith(page.url) || hover) && {
+          opacity: '1',
+          backgroundColor: 'hsla(0, 0%, 50%, 0.15)',
+        }),
+      }}
+      {...(location.href.endsWith(page.url) && {
+        'aria-current': 'location',
+      })}
+    >
+      {(page.nav && page.nav.key) || page.url}
+    </a>
   );
 };

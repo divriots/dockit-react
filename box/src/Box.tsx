b152b2b9d3be27a8ffc167229d72a6f74414d9ab
mveres <veres85@yahoo.com>
2022-05-02 16:29:16 +0300

    box props

--- box/src/Box.tsx
+++ box/src/Box.tsx
@@ -2,24 +2,16 @@ import '@divriots/dockit-core/box/dockit-box.define.js';
 import '@divriots/dockit-core/style.css';
 import React, { CSSProperties, ReactNode } from 'react';
 
-interface BoxProps {
+type BoxProps = {
   checkeredBackground?: boolean;
   className?: string;
-  style?: CSSProperties;
-  children?: ReactNode | ReactNode[];
-}
+} & JSX.IntrinsicAttributes &
+  Partial<any>;
 
-export const Box = ({
-  checkeredBackground,
-  className,
-  style = {},
-  children,
-}: BoxProps) => (
+export const Box = ({ checkeredBackground, className, ...rest }: BoxProps) => (
   <dockit-box
     class-name={className}
-    style={style}
     checkered-background={checkeredBackground}
-  >
-    {children}
-  </dockit-box>
+    {...rest}
+  />
 );
